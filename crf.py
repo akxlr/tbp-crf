2e601a66bcc47d7257d79761b88f5b631a01f087
Andrew Wrigley <andrew@wrigley.io>
2018-03-05 10:38:51 +1100

    Add Ye Nan's code for reading labels and tree decomposition

--- crf.py
+++ crf.py
@@ -5,14 +5,68 @@ import time
 import numpy as np
 import sys
 import tbp
-from tbp import Graph, Factor
+from tbp import Graph, Factor, DecomposedGraph, DecomposedFactor
 import matplotlib
 from matplotlib import pyplot as plt
 
-
 LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 
-def read_pot(filename, subset=None) -> List[Graph]:
+# View the factor as a tree by treating each index as a path to a non-default
+# value, and filling in branches with default values, with identical branches
+# merged. 
+# This function recursively traverse the tree to construct a tensor
+# decomposition.
+def construct_rank1_tensors(tensors, weights, factor, indices, depth):
+    tensor = []
+    for d in range(depth):
+        v = np.zeros(factor.cardinalities[d])
+        v[indices[0][d]] = 1.
+        tensor.append(v)
+
+    # at a leaf node (tip of a non-default branch)
+    if depth == factor.n_vars: 
+        assert len(indices) == 1
+        tensors.append(tensor)
+        weights.append(factor.table[tuple(indices[0])])
+    # at an internal node
+    else:
+        # first construct tensor for the branch with default value
+        unique_values = np.unique(indices[:,depth])
+        v = np.ones(factor.cardinalities[depth])
+        v[unique_values] = 0.
+        tensor.append(v)
+
+        for d in range(depth+1, factor.n_vars):
+            v = np.ones(factor.cardinalities[d])
+            tensor.append(v)
+
+        tensors.append(tensor)
+        weights.append(1.)
+
+        # process each remaining branch separately
+        for u in unique_values:
+            construct_rank1_tensors(tensors, weights, factor,
+                    indices[np.where(indices[:,depth] == u)], depth+1)
+
+def decompose_sparse_factor(factor):
+    table = factor.table
+    # construct a decomposition for the single-variable case
+    if table.ndim == 1: 
+        n_terms = 1
+        weights = np.ones(n_terms)
+        matrices = [table.reshape(table.shape[0], 1)]
+    # construct a decomposition for (sparse) higher order factors
+    else: 
+        indices = np.transpose(np.where(table != 1.0))
+        weights = [] 
+        tensors = []
+        construct_rank1_tensors(tensors, weights, factor, indices, 0)
+        matrices = [np.array(m).transpose() for m in np.swapaxes(tensors, 0, 1).tolist()]
+        for i in range(factor.n_vars):
+            assert matrices[i].shape == (factor.cardinalities[i], len(weights))
+    return DecomposedFactor(factor.vars, weights, matrices)
+
+def read_pot(filename, temp=1., decompose=False, subset=None): # -> List[Graph] or List[DecomposedGraph]:
     """
     Read Ye Nan's .pot format and return the CRFs as a list of Graph instances (each .pot file contains several CRFs
     separated by a blank line). The original files contain ^T characters, these should be replaced with spaces first
@@ -36,7 +90,12 @@ def read_pot(filename, subset=None) -> List[Graph]:
                 # the next graph.
                 if factors.values():
                     if not subset or n in subset:
-                        yield Graph(factors.values())
+                        if decompose:
+                            g = Graph(factors.values())
+                            adjust_temperature(g, temp)
+                            yield DecomposedGraph([decompose_sparse_factor(factor) for factor in g.factors])
+                        else:
+                            yield Graph(factors.values())
                         if n == sorted(subset)[-1]:
                             return
                     n += 1
@@ -69,7 +128,7 @@ def read_pot(filename, subset=None) -> List[Graph]:
 
     # print("Read {} CRFs".format(len(gs)))
     # return gs
-
+ 
 def read_marg(filename, subset=None):
     """
     Read Ye Nan's .marg format and return a list of graph marginals, each of which is a list of lists (each .mar file
@@ -105,6 +164,19 @@ def read_marg(filename, subset=None):
 
     # print("Read marginals for {} graphs".format(len(all_marginals)))
     # return all_marginals
+
+def read_label(filename, subset=None):
+  seqs = []
+  seq = []
+  with open(filename, 'r') as f:
+    for line in f.readlines():
+      if line.strip() == '':
+        seqs.append(seq)
+        seq = []
+      else:
+        seq.append(line.strip().split()[0])
+  return np.array(seqs) if subset == None else np.array(seqs)[subset]
+
 
 def adjust_temperature(g: Graph, temp):
     for factor in g.factors:
@@ -181,16 +253,22 @@ def run_tests(test_group, rs, ks, temps=None, subset=None):
 
     graph_filename = 'hocrf/fold0-{0}/fold0-{0}.pot'.format(test_group)
     marg_filename = 'hocrf/fold0-{0}/fold0-{0}.marg'.format(test_group)
+    label_filename = 'hocrf/labels/fold0-{0}.ts'.format(test_group)
 
     # Results format will be {temp -> {r -> [{k -> [err, infer_time]}, decomp_time]}}
     # Initially, err, infer_time and decomp_time will be lists - later we take the average
     all_results = {}
+    use_tree_decomposition = True
     for temp in temps:
         results = {}
         for r in rs:
             results[r] = [{}, []]
-            for i, (g, true_marg) in enumerate(zip(
-                    read_pot(graph_filename, subset=subset), read_marg(marg_filename, subset=subset))):
+            tot_len = 0
+            for i, (g, true_marg, seq) in enumerate(zip(
+                    read_pot(graph_filename, temp=temp, decompose=use_tree_decomposition, subset=subset), 
+                    read_marg(marg_filename, subset=subset),
+                    read_label(label_filename, subset=subset),
+                    )):
                 print("Graph {} (#{})".format(i+1, subset[i]))
 
                 # Check number of variables and variable cardinalities match between .pot and .marg
@@ -199,12 +277,17 @@ def run_tests(test_group, rs, ks, temps=None, subset=None):
                 assert all(x == len(LETTERS) for x in cardinalities), "Graph {}: .pot cardinalities are {}, expected all {}".format(i, cardinalities, len(LETTERS))
                 assert all(len(x) == len(LETTERS) for x in true_marg), "Graph {}: .marg cardinalities are {}, expected all {}".format(i, [len(x) for x in true_marg], len(LETTERS))
 
-                adjust_temperature(g, temp=temp)
 
-                t0 = time.time()
-                dg = g.decompose(r=r)
-                decomp_time = time.time() - t0
+                if use_tree_decomposition:
+                    dg = g
+                    decomp_time = 0. # very fast...
+                else:
+                    adjust_temperature(g, temp=temp)
+                    t0 = time.time()
+                    dg = g.decompose(r=r)
+                    decomp_time = time.time() - t0
                 results[r][1].append(decomp_time)
+                tot_len += len(seq)
 
                 for k in ks:
                     if k not in results[r][0]:
@@ -213,10 +296,17 @@ def run_tests(test_group, rs, ks, temps=None, subset=None):
                     marg_est = dg.tbp_marg(k=k)
                     infer_time = time.time() - t0
                     # err = tbp.l1_error(marg_est, true_marg)
-                    err = classification_accuracy(marg_est, true_marg)
+                    # err = classification_accuracy(marg_est, true_marg)
+                    pred = np.array(list(LETTERS))[np.array(marg_est).argmax(axis=1)]
+                    err = (pred != seq).sum()
                     results[r][0][k][0].append(err)
                     results[r][0][k][1].append(infer_time)
-                    print('    temp={}, r={}, k={}: {}'.format(temp, r, k, err))
+                    if use_tree_decomposition:
+                        print('    temp={}, k={}, err={}, infer_time={}'
+                            .format(temp, k, err, infer_time))
+                    else:
+                        print('    temp={}, r={}, k={}, err={}, decomp_time={}, infer_time={}'
+                            .format(temp, r, k, err, decomp_time, infer_time))
         all_results[temp] = results
 
     # Take averages
@@ -224,33 +314,39 @@ def run_tests(test_group, rs, ks, temps=None, subset=None):
         for r in rs:
             for k in ks:
                 # err
-                all_results[temp][r][0][k][0] = np.mean(all_results[temp][r][0][k][0])
+                all_results[temp][r][0][k][0] = np.sum(all_results[temp][r][0][k][0]) / tot_len
                 # infer_time
                 all_results[temp][r][0][k][1] = np.mean(all_results[temp][r][0][k][1])
             # decomp_time
             all_results[temp][r][1] = np.mean(all_results[temp][r][1])
 
+    print(all_results)
     return all_results
 
 
 if __name__ == '__main__':
     test_group = sys.argv[1]
     limit = 20
-    temps = [1, 0.5, 0.1]
+    #temps = [1, 0.5, 0.1]
+    temps = [1]
 
     # Choose which graphs to test at random (just taking the first `limit` graphs from the start of the files is
     # not fair, because they are different handwriting examples of the same word). Rather than load all graphs
     # into memory and take a subset, we choose which graphs to load in advance to save memory.
     n_graphs = 6250  # Number of graphs in the file
     indices = sorted(random.sample(range(n_graphs), limit))
+    #indices = range(n_graphs)
 
     res = run_tests(
         test_group,
-        rs=[2, 5, 10, 100, 1000],
-        ks=[10, 100, 1000, 10000, 100000], #, 1000000],
+        #rs=[2, 5, 10, 100, 1000],
+        rs=[100],
+        #ks=[10, 100, 1000, 10000, 100000], #, 1000000],
+        ks=[10000], #, 1000000],
         temps=temps,
         subset=indices,
     )
     for temp in temps:
         save_plot(res[temp], 'fold0-{}-random{}-t={}'.format(test_group, limit, temp))
 
+# vim: softtabstop=4 shiftwidth=4 tabstop=4 expandtab
